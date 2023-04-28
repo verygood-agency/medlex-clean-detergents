2d69b31d9dfaf5ff5cca2af1a72bb9a570d01a49
RoninsWork <130744166+verygood-agency@users.noreply.github.com>
2023-04-28 16:35:15 +0300

    заканчиваю отладку анимации (удалил паралакс эффект)

--- src/js/main.js
+++ src/js/main.js
@@ -1,39 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // Float animation
+  function floatAnimation(layer) {
+    const amplitude = 5;
+    const period = parseFloat(layer.getAttribute('data-float-period')) * 1000;
+    const startTime = performance.now();
+
+    function updateFloat() {
+      const elapsedTime = performance.now() - startTime;
+      const progress = (elapsedTime % period) / period;
+      const angle = progress * 2 * Math.PI;
+      const y = -amplitude * Math.sin(angle);
+
+      layer.style.transform = `translateY(${y}px)`;
+
+      requestAnimationFrame(updateFloat);
+    }
+
+    requestAnimationFrame(updateFloat);
+  }
+
   // техническая часть - УДАЛИТЬ НА ПРОДАКШЕНЕ!
   // получить в консоли элемент, по которому кликнули
   document.addEventListener('click', e => console.log(e.target));
 
-
   const imgGroup = document.querySelector('.first-screen__bg-images');
-const imgPositions = {};
 
-function updateImgPositions() {
+  // Запускаем анимацию для всех слоев
   imgGroup.querySelectorAll('.layer').forEach(layer => {
-    imgPositions[layer] = {
-      x: layer.offsetLeft + layer.offsetWidth / 2,
-      y: layer.offsetTop + layer.offsetHeight / 2,
-    };
+    floatAnimation(layer);
   });
-}
-
-function parallax(e) {
-  imgGroup.querySelectorAll('.layer').forEach(layer => {
-    const speed = layer.getAttribute('data-speed');
-    const pos = imgPositions[layer];
-    const x = (pos.x - e.pageX) * speed / 100;
-    const y = (pos.y - e.pageY) * speed / 100;
-
-    layer.style.transform = `translate(${x}px, ${y}px)`;
-  });
-};
-
-if (screen.width > 1024) {
-  updateImgPositions();
-  document.addEventListener('mousemove', parallax);
-  window.addEventListener('resize', updateImgPositions);
-}
-
 
 });
-
-
