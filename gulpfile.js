7254f692ce8ad9dc4431e7189b6183178a97a584
RoninsWork <130744166+verygood-agency@users.noreply.github.com>
2023-05-02 15:08:47 +0300

    Удаляю файлы сборщика и добавляю в проект анимацию

--- gulpfile.js
+++ gulpfile.js
@@ -11,12 +11,6 @@ const gcmq = require('gulp-group-css-media-queries');
 const order = require('gulp-order');
 const sassGlob = require('gulp-sass-glob');
 
-
-gulp.task('cleaner', function () {
-  return gulp.src('dist/**/*', { read: false })
-    .pipe(rm())
-})
-
 // Обработка HTML
 gulp.task('html', function () {
   return gulp.src(['src/html/*.html'])
@@ -29,26 +23,9 @@ gulp.task('html', function () {
     .pipe(fileInclude({
       basePath: 'src/html/sections/'
     }))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('./'));
 });
 
-// Обработка IMG
-gulp.task('img', function () {
-  return gulp.src('src/img/**/*')
-    .pipe(gulp.dest('dist/img'));
-});
-
-// Обработка fonts
-gulp.task('fonts', function () {
-  return gulp.src('src/fonts/**/*')
-    .pipe(gulp.dest('dist/fonts'));
-});
-
-// Обработка libs
-gulp.task('libs', function () {
-  return gulp.src('src/libs/**/*')
-    .pipe(gulp.dest('dist/libs'));
-});
 
 // Обработка SCSS
 gulp.task('scss', function () {
@@ -61,34 +38,18 @@ gulp.task('scss', function () {
     }))
     .pipe(cleanCSS())
     .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('dist/css'));
-});
-
-
-gulp.task('js', function () {
-  return gulp.src('src/js/*.js')
-    .pipe(order([
-      'js/parts/*.js',
-      'src/js/*.js'
-    ]))
-    .pipe(concat('main.js'))
-    .pipe(uglify())
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('dist/js'));
+    .pipe(gulp.dest('css'));
 });
 
 // Отслеживание изменений
 gulp.task('watch', function () {
   gulp.watch('src/html/*.html', gulp.series('html'));
   gulp.watch('src/html/sections/**/*.html', gulp.series('html'));
-  gulp.watch('src/html/items/**/*.html', gulp.series('html'));
+  gulp.watch('src/html/bem/**/*.html', gulp.series('html'));
+  gulp.watch('src/html/base/**/*.html', gulp.series('html'));
   gulp.watch('src/scss/**/*.scss', gulp.series('scss'));
-  gulp.watch('src/js/**/*.js', gulp.series('js'));
-  gulp.watch('src/img/**/*', gulp.series('img'));
-  gulp.watch('src/libs/**/*', gulp.series('libs'));
-  gulp.watch('src/fonts/**/*', gulp.series('fonts'));
 });
 
 // Задача по умолчанию
-gulp.task('default', gulp.series('cleaner', 'html', 'libs', 'fonts', 'img', 'scss', 'js', 'watch')
+gulp.task('default', gulp.series('html', 'scss', 'watch')
 );
---
+++ js/main.js
@@ -0,0 +1,118 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // Float animation
+  function floatAnimation(layer) {
+    const amplitude = 5;
+    const period = parseFloat(layer.getAttribute('data-float-period')) * 1000;
+    const startTime = performance.now();
+
+    function updateFloat() {
+      const elapsedTime = performance.now() - startTime;
+      const progress = (elapsedTime % period) / period;
+      const angle = progress * 2 * Math.PI;
+      const y = -amplitude * Math.sin(angle);
+
+      layer.style.transform = `translateY(${y}px)`;
+
+      requestAnimationFrame(updateFloat);
+    }
+
+    requestAnimationFrame(updateFloat);
+  }
+
+  // техническая часть - УДАЛИТЬ НА ПРОДАКШЕНЕ!
+  // получить в консоли элемент, по которому кликнули
+  document.addEventListener('click', e => console.log(e.target));
+
+  const imgGroup = document.querySelector('.first-screen__bg-images');
+
+  // Запускаем анимацию для всех слоев
+  imgGroup.querySelectorAll('.layer').forEach(layer => {
+    floatAnimation(layer);
+  });
+
+
+  gsap.utils.toArray('.product-card__photo img').forEach(photo => {
+    gsap.from(photo, {
+      opacity: 0,
+      scale: 0.5,
+      // translateY: 50,
+      translateX: 50,
+      duration: 1,
+      ease: 'power3.out',
+      scrollTrigger: {
+        trigger: photo,
+        start: 'top 90%',
+        end: 'bottom 20%',
+        scrub: true,
+        toggleActions: 'play none none reverse',
+      },
+    });
+  });
+
+  gsap.utils.toArray('.product-card__details').forEach(photo => {
+    gsap.from(photo, {
+      opacity: 0,
+      scale: 0.5,
+      translateX: -50,
+      // translateX: 50,
+      duration: 1,
+      ease: 'power3.out',
+      scrollTrigger: {
+        trigger: photo,
+        start: 'top 90%',
+        end: 'bottom 10%',
+        scrub: true,
+        toggleActions: 'play none none reverse',
+      },
+    });
+  });
+
+  gsap.fromTo('.first-screen__logo', {
+    opacity: 0,
+    y: -50,
+  }, {
+    opacity: 1,
+    y: 0,
+    duration: 1,
+    ease: 'power3.out',
+  });
+
+  gsap.fromTo(['.first-screen .title', '.first-screen .subtitle'], {
+    opacity: 0,
+    y: -50,
+  }, {
+    opacity: 1,
+    y: 0,
+    duration: 1,
+    ease: 'power3.out',
+    stagger: 0.2,
+  });
+
+  gsap.fromTo('.advantages__item', {
+    opacity: 0,
+    y: 50,
+  }, {
+    opacity: 1,
+    y: 0,
+    duration: 1,
+    ease: 'power3.out',
+    stagger: 0.2,
+  });
+
+  // задаем начальное положение для изображений
+  gsap.set('.first-screen__bg-images img', { y: -100, opacity: 0 });
+
+  // анимируем появление изображений
+  gsap.fromTo('.first-screen__bg-images img', {
+    opacity: 0,
+    y: -100,
+  }, {
+    opacity: 1,
+    y: 0,
+    duration: 1,
+    ease: 'power3.out',
+    stagger: 0.2,
+  });
+
+
+});
